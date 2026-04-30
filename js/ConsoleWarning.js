bb1cad03359c8bc0108734177fba582bf035f70e
OXOP <minbartek41@gmail.com>
2026-04-30 21:31:04 +0200

    Final update before sleeping - no skids this time and stuff and minified the meows for maximum unexpecticness

---
+++ js/ConsoleWarning.js
@@ -0,0 +1,33 @@
+const warningTitle = `
+          _______  _        ______              _______  _ 
+|\\     /|(  ___  )( \\      (  __  \\   |\\     /|(  ____ )( )
+| )   ( || (   ) || (      | (  \\  )  | )   ( || (    )|| |
+| (___) || |   | || |      | |   ) |  | |   | || (____)|| |
+|  ___  || |   | || |      | |   | |  | |   | ||  _____)| |
+| (   ) || |   | || |      | |   ) |  | |   | || (      (_)
+| )   ( || (___) || (____/\\| (__/  )  | (___) || )       _ 
+|/     \\|(_______)(_______/(______/   (_______)|/       (_)
+`;
+
+const warningBody = `>> WARNING: UNAUTHORIZED ACCESS DETECTED
+>> stop pasting scripts u found on discord
+>> zero accounts here but u will look like a skid
+>> busy with black midi + buisness (zero grammar)
+>> close this or get back to work!!`;
+
+const titleStyle = "color: #ff0000; font-family: monospace; font-weight: bold; text-shadow: 0 0 5px #ff0000; line-height: 1.1;";
+
+const bodyStyle = "color: #00ff2a; font-family: monospace; font-size: 14px; font-weight: bold; line-height: 1.5;";
+
+const signatureStyle = `
+    background: linear-gradient(to right, #ff0000, #00ff2a);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    font-size: 32px;
+    font-weight: 900;
+    font-family: sans-serif;
+`;
+
+console.log("%c" + warningTitle, titleStyle);
+console.log("%c" + warningBody, bodyStyle);
+console.log("%c- OXOP", signatureStyle);
